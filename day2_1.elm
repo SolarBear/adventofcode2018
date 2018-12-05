114d192c102e6f0bbad29802025dc0102f03d0fa
SolarBear <7608394+SolarBear@users.noreply.github.com>
2018-12-04 22:19:55 -0500

    Day 2, part 1 done.

---
+++ day2_1.elm
@@ -0,0 +1,309 @@
+import Html exposing (..)
+import Browser exposing (sandbox)
+import String exposing (..)
+
+
+-- MODEL
+
+input = """pnebjqralgdgckzfifvtxywomu
+pnebjqsalrdgcqzfihotxhwomu
+pneajqsalrdgckzfihytxywoml
+pnepjqsalrwgckztihvtxywomu
+pnhbjqsalrdgckzfimvtxywodu
+pnwbjqsdlrdgckzfihvnxywomu
+inebjqnalrdgckzfihvtxzwomu
+pnebjssalhdgckzfihvtsywomu
+pnebjqjalrdgckzfiavtxywoku
+vnebjqsalrdgckzfihvbxmwomu
+phebjksaurdgckzfihvtxywomu
+pneojqealrdgckzhihvtxywomu
+snebjqsalrdgckzqihvtxyzomu
+pnebjqsalrtackzfihvtxswomu
+bnebjqlalrdgckzfihvtxywhmu
+pnebjqfalrdgckzfijvtxywomi
+fnehjbsalrdgckzfihvtxywomu
+pnebjasalrdgckzdihvtxqwomu
+pnebjhsaljdgckzfihvtxywmmu
+pnebjqsalrdgckzfihvsxykoau
+pnebjqsalrdgckzbihvtdywomc
+pnobjqsalrxgckzfihvtxywomh
+pnebjqstlrdgchzfihvtxywnmu
+pnebjquaxrdgckzfihvtxywolu
+pqebjqsalrdgcdzfihvtcywomu
+xnabjqsalrdgckzfihvtxywmmu
+rnebjqsalrdgckzfihvtxmwouu
+vaebjqsalrdgckcfihvtxywomu
+pnebjqsalrpgcnzfihvbxywomu
+pcvbjqsalrdjckzfihvtxywomu
+pneyjqsafrdgckzfihdtxywomu
+pxedjqsalrdgckzfihvtxyzomu
+pnebjqsalrdgctzfihnyxywomu
+pnebjqsalrdgckzfihvtnylsmu
+pnebjqsalrdyckzfihvbxycomu
+fnebjqsalrdgckzfihvtxtwomc
+pnobjqsalrdgckdfihvtxywomh
+pqebjqsalrdgcqzfihvtxywymu
+pnebxqsalrdgckzficvtwywomu
+pnebjqshlragczzfihvtxywomu
+pnebqqsalrdackzfihttxywomu
+pnebjqsalrdsckwfbhvtxywomu
+pnehjqsalrdgcuzfxhvtxywomu
+pnebjqsavrdgckzfihvexywomn
+pnebunsalrdgckzfihvtxywomi
+pnebjxsalrdgckzfmhvtpywomu
+rnebjqsalrdghkzfihztxywomu
+pnebjqsalrigcbzfihvfxywomu
+pnebqqsalrggckzfihvtxyromu
+pnebjqsalrdgchzfihvtxylmmu
+pnebeqsalrdgckzdihvtxywoms
+pnebjqsalrdgckzzihvfxywozu
+pnzbjgsalrtgckzfihvtxywomu
+pnebjqsaledgckzjihvtxzwomu
+pnebjqsalydgckqfihvtxywouu
+pnebjqsalrdgckufihvqxdwomu
+pnebjqsylrdgckzfihvdxyjomu
+pnemjqsalrdgckzeihvtxywoqu
+plebjasalrdgckzfihvtxywomb
+pnebjqsadrdgckufihvtxyfomu
+pbebjqsaardgckzfihvtxmwomu
+pnebjqsalrdgcmzfihotxywgmu
+pnebjqsaprdgcizfihvtxywhmu
+pnebjqsalrkgcuzfihvtlywomu
+pnebjqsalrdnckzfihvtxysomg
+pnebjqdafrdgckzfihctxywomu
+pnebjqsalrdgckzfihutxkwomp
+pnebvqsalrdgclzfimvtxywomu
+pnebjqralrdgcktfihvtxiwomu
+pneujqsalrdsckzfzhvtxywomu
+pnebfqgalrdgckzfihvtxywjmu
+pneyjqsalrkgckzfihctxywomu
+pndbjqsalrdgckzfjhvtxywouu
+pneljnsalrdgcozfihvtxywomu
+phebjqsalrdgckzfihxtxdwomu
+pnlbjqsalrhgckzfzhvtxywomu
+pnebjqsalrsgckzfiovtxywwmu
+pncbjqsalrdgfkzfivvtxywomu
+nnebjqsalrdgckzfthvtxycomu
+pnebjqwalsdgckzfixvtxywomu
+pnebjtsalrdgcfzfimvtxywomu
+pnebjqsvlrdgckzfihutxfwomu
+pnebjmsalrdgckzkxhvtxywomu
+pnekjqsllrdgckzfinvtxywomu
+pneijqsxlrdgckzfihvtxywjmu
+wnxbjqsafrdgckzfihvtxywomu
+pnebjqskledgokzfihvtxywomu
+pnebjqvalrdgckzfihvtxytoju
+pneqjqsalrdgckzfilvthywomu
+pnebjqsalrdgckzfihvokywomf
+bnebjqsalrdgckufihvtxywimu
+pnebjqsaurdgckzfihvtrywosu
+pnebjmsaludgckzfihvtxywomn
+pnebdqsalrdgcktfihvtxywodu
+pnebjqjylzdgckzfihvtxywomu
+piebjqsalrdgcrzfihstxywomu
+pnebjqsaurdgckwfnhvtxywomu
+pnebxqsajrdgcjzfihvtxywomu
+pnebjqsalrdghsdfihvtxywomu
+pnebcqsxlrdgckzfihvtxyaomu
+pnefjqsalrdgckzfuhvtxyworu
+pnebjqsalrdlcksfihvteywomu
+pnebjqlalrgackzfihvtxywomu
+pnebdqsalrdickzfihvtxdwomu
+pneujksalrdgctzfihvtxywomu
+pnebjqsalrduckzfihvsxywomf
+pnebjqsalrdgckcfihotxywomd
+envbjqsalsdgckzfihvtxywomu
+pnebjqsalzdgcvzzihvtxywomu
+pnebjqsalrdyckzflhvyxywomu
+pnebjqsalrdglkzfihstxymomu
+pnebmqsalrdgokzfihvtxywoml
+pnebjqsylrdnckzfihatxywomu
+pnebjqaflndgckzfihvtxywomu
+pneboqsagragckzfihvtxywomu
+peebjqstlndgckzfihvtxywomu
+onebjqsklrdgckzfihvtxmwomu
+pnebjqjnlrdgckrfihvtxywomu
+pnebjqsalrhgckzfihvqxywomh
+pnebjqsalrdgckzzihvtxowomw
+pnebjgsalrdgckffihltxywomu
+znebaqsalcdgckzfihvtxywomu
+pnnbjqeasrdgckzfihvtxywomu
+rnebjqaalrxgckzfihvtxywomu
+pnebjqsalrdgckaxphvtxywomu
+pnebjcnalrdgnkzfihvtxywomu
+pnebjasalbdgckzmihvtxywomu
+pnebjqsalrdgckefjhvtmywomu
+pnebjqsalrdgmkzfihvtxyoomb
+pnebjqsalrkgckogihvtxywomu
+pnwbjqsalrdgckztihvtxywomt
+pnebjqsalrdgckzfihotgnwomu
+pnebjqsdlrrgckzfihvtxyaomu
+pnebvasalrdgckzfihvtsywomu
+pnebrqqalrvgckzfihvtxywomu
+tnebjqsglrdgqkzfihvtxywomu
+pnebjqsatrsgckifihvtxywomu
+pneboqsalrdgckzfihvkxywomi
+pnezaqsalrdgcktfihvtxywomu
+pnebjqsnlrdgckzfihvfxqwomu
+pneajqsaxrmgckzfihvtxywomu
+pnebjosalodgckzfihvxxywomu
+pnebjqsalndgckmfihvtfywomu
+pneejqsalidgckzfihgtxywomu
+pnecjqsalrdgckzfihptxiwomu
+tnebjqsalrdgckznihvxxywomu
+ptebjqsalrdgckzfimvtxywomm
+wnebjqsalndgckzfihvtxywoju
+fnebmqsplrdgckzfihvtxywomu
+pnlbjqsalrdghkzficvtxywomu
+pnebjqsesrdgckzdihvtxywomu
+pnebjqsalregokzfirvtxywomu
+pnebjtualrtgckzfihvtxywomu
+pnebjwsdlrdgckzfihvtxywoml
+pnlbjqsayrdgckzfqhvtxywomu
+pnebjwsalpdgckzfihvtxywomc
+pnqbjqsalcdgckzhihvtxywomu
+pneujqsalrdgckzfhhvtxrwomu
+pnebjqsalqdgcizfihvtxywimu
+pnebjqsacldgckzfihvwxywomu
+puebjqsalrdgckzfbhvtxyeomu
+pnebjqsalrdgcyimihvtxywomu
+pnebjlsalrdgckzfihvtxiwome
+pnebfusalrdgckzfihvtxywodu
+pnebjqsalrdgvazfirvtxywomu
+pnebjqsalrdgckyfohvtxywomz
+gnenjqsalrdgckzfihvtxynomu
+mnebjqsalrdgckhfihvtxycomu
+phebjqsalrdgckzfihvtxtworu
+pnebjqsalrdgdkzfihvtxywfmj
+pneveqsairdgckzfihvtxywomu
+pnebjqsalcdlckzfihvtxywomg
+pneajqsalrdgckzfihvtxygoxu
+puebjqdclrdgckzfihvtxywomu
+tuebjqsalrdgckzfihvtxywoou
+pwenjqsalrdgckzfihvtxywomg
+pnebjqsalrdgckzfihhltywomu
+pnebjqsalrdgchzqievtxywomu
+pnegjqsalrdgckzfiovtxywdmu
+pnebjaralrqgckzfihvtxywomu
+pnebjqsalrdrckzfimvtxywomm
+pnebjqsalrdgckzfpgvtxewomu
+pnebjqsalrdhcqzfihitxywomu
+pnebjqsalrjgckefihmtxywomu
+pnebjcsalrdgcksfikvtxywomu
+pnebjqsalrdgckzfihvtxywdjc
+pnebjqsazrjgckzjihvtxywomu
+pnfbjqsclrdgckzfihvtxybomu
+pnebjqsalrdgckuqihvtxyaomu
+pfpbjzsalrdgckzfihvtxywomu
+pnevjqsalrdgckwfihytxywomu
+pnebjqsqlrkgckzfihvtvywomu
+pneejqsalrdlckzfihvtxywopu
+pnebjqsalcdgxkzfihvtxywomd
+pneqjqsalrdgcvzzihvtxywomu
+pnvbjqsalydgctzfihvtxywomu
+pnebjqsalrdgckzzihvfxywomn
+pnybjqsaerdgckzfihstxywomu
+pnobjqsalrdkckzfihvtxywomv
+pnebjqsalridckzfihvtxywfmu
+pnhbjqsaludgckyfihvtxywomu
+pnetjqsaprdgykzfihvtxywomu
+wnebjqsalrdvcfzfihvtxywomu
+pnetjqsalrdmckwfihvtxywomu
+pnebjysalrdgcszfihvtxnwomu
+pnebjqsrlrdgckzfihvtxywkhu
+pnubjqsplrdgcjzfihvtxywomu
+pnebjqsalrdzckzficjtxywomu
+pnebjqsalregckzfinvtxywoku
+pnebjqsalrcgckyfivvtxywomu
+pyenjqsalrdgckzfihvnxywomu
+prebjqsalrdnckzfihvtxysomg
+pnebjnsalrdgchzfihvaxywomu
+pnebjqsalrdgckzfihxagywomu
+pnebjqsalrdgckzvihvtoywoml
+pnebjqsilrdgckzfihvtfywgmu
+pnebjqmalrdgckzfihvtvawomu
+pnebqqsalrdgckzfiuvtfywomu
+pneqjqsalrdgckzfihvqxywomi
+pnebjesalrsgckzfihvtxywmmu
+znebjqsblrdgckzfihvlxywomu
+pnebjqsalrdgckzfuhvtlyworu
+pnebjqsylrdgckzfihvqxpwomu
+onebjqsalfdgckifihvtxywomu
+pnebjusalrdgckzfihvtxywyml
+pnebjssflrdgckzfigvtxywomu
+pnebjfsdzrdgckzfihvtxywomu
+pnebjqsalrdgcktfihvixywocu
+gnebjqnaqrdgckzfihvtxywomu
+pnebjqsaqrugckzfihhtxywomu
+pnebjqsxlrdgckzfihvtxlwosu
+pnebjzsalrdgckzmihvtxywovu
+pnebgqsalrdgckzfizvtxyjomu
+pnebjqsmlrdgckzfihvtxywsmi
+pnebjqsakmdgckzjihvtxywomu
+pnebjqdglrdgckvfihvtxywomu
+pnebmhsalrdgckxfihvtxywomu
+pneejqsalrdlckzfihvnxywomu
+bnebjqsalmdgckzfihvfxywomu
+bnebjnsalrdgcizfihvtxywomu
+pnebjqsalhdgcdzfihvbxywomu
+pnebjqsjlrdgckzfihvgiywomu
+pnebjisalrdgckzfihvtxywqmi
+pdebjqsalrdickzfihhtxywomu
+pnebjqsalrdkckzfihvjeywomu
+pneyjqsalrqgckzfihvtxywohu
+pnebjqsalrdgckcfihvtxjlomu
+plebqwsalrdgckzfihvtxywomu
+pnebjqlalrdgckzfihetxynomu
+sngbjqsalrdgckzfihvmxywomu"""
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+type alias Model = String
+
+init : Model
+init =
+    input
+
+
+-- UPDATE
+
+type Msg = Compute
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    let
+        sumText = transform model
+                |> String.fromInt
+    in
+        p [] [ text sumText ]
+
+transform : String -> Int
+transform val =
+    let
+        lines = String.lines val
+        sortedLines = List.map String.toList lines |> List.map List.sort |> List.map String.fromList
+        twos = List.filter (isCharacterRepeatedNTimes 2) sortedLines |> List.length
+        threes = List.filter (isCharacterRepeatedNTimes 3) sortedLines |> List.length
+    in
+        twos * threes
+
+isCharacterRepeatedNTimes : Int -> String -> Bool
+isCharacterRepeatedNTimes n str =
+    if String.isEmpty str then
+        False
+    else
+        let
+            firstChar = String.left 1 str
+            charIndices = String.indexes firstChar str |> List.length
+        in
+            if charIndices == n then
+                True
+            else
+                isCharacterRepeatedNTimes n (String.dropLeft charIndices str) 
